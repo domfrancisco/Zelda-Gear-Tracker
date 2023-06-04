ef6e41d14c3e8addd219c9f45556e032d26c5d11
Nico <=>
2023-06-04 09:03:15 -0400

    created new classes and laid out pseudo code

--- js/clothes.js
+++ js/clothes.js
@@ -1,10 +1,19 @@
-class clothingOption = {
-    constructor(set,head,legs,body,upgrade){
-        this.set = set;
+// each clothing set will have their own set of clothing items
+// head,leg,and body will be clothingItem objects
+class clothingSet{
+     constructor(set, head,legs,body){
+        this.setName = set;
         this.head = head;
         this.leg = legs;
         this.body = body;
-        this.upgrade = upgrade;
+    }
+}
+
+// each clothingItem has an upgrade list of items, the aquired bool, 
+// and an upgradeLevel. Each method is a distinct action you can take 
+class clothingItem {
+    constructor(upgradeList,aquired){
+        this.upgradeList = upgradeList;
         this.aquired = false;
         this.upgradeLevel = 0;
     }
@@ -18,10 +27,38 @@ class clothingOption = {
     }
 
     GetItemsForNextLevelUp(){
-        return this.upgrade[upgradeLevel];
+        return this.upgradeList[upgradeLevel];
     }
 }
 
+
+// change the upgrade array to an array of item objects
+class Item{
+    constructor(quantityRequired,itemName){
+        this.quantityRequired = quantity; // number of items required
+        this.quantityObtained = 0; // number of items obtained
+        this.itemName = itemName;
+        this.aquired = false;
+    }
+}
+
+
+class ShoppingList{
+    shoppingList = [];
+
+    ChangeQuantityOfItem(inputItem,quantityChangeNumber){
+        // loop through shopping list and change the quantity of that item
+        // would do something like this pseudo code
+        /*
+            for each item in shoppingList{
+                if (item matches inputItem){
+                    shoppingList[item].quantityObtained += quantityChangeNumber
+                }
+            }
+        */
+    }
+
+}
 
 const clothing = [
     {
